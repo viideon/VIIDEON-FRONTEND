87eee69af43cf60e81413adecfd9ec95ae56dbc3
Abdul Basit <basitsattar850@gmail.com>
2020-08-19 15:58:32 +0500

    added dummy assets in music assets

--- src/Screens/MusicAssets/index.tsx
+++ src/Screens/MusicAssets/index.tsx
@@ -12,12 +12,25 @@ interface IProps {
   deleteAsset: (assetId: any) => void;
   isDeletingAsset: boolean;
 }
+
 class MusicAssets extends React.Component<IProps> {
+  state = {
+    dummyMusic: [
+      "https://videonpro.s3-us-west-1.amazonaws.com/assets/musicasset1.mp3",
+      "https://videonpro.s3-us-west-1.amazonaws.com/assets/musicasset2.mp3"
+    ]
+  };
   render() {
     return (
       <Home>
         <h3 className="assetHeading">Music Assets</h3>
-        <Grid container style={{ padding: "5px 20px 20px 20px" }}></Grid>
+        <Grid container style={{ padding: "5px 20px 20px 20px" }}>
+          {this.state.dummyMusic.map((audioUrl: string, index: number) => (
+            <Grid item xs={12} sm={12} md={12} lg={12} key={index}>
+              <MusicAsset audioUrl={audioUrl} />
+            </Grid>
+          ))}
+        </Grid>
       </Home>
     );
   }
@@ -35,59 +48,29 @@ class MusicAssets extends React.Component<IProps> {
 // };
 export default MusicAssets;
 
-// const Asset = ({ asset, deleteAsset, isDeletingAsset }: any) => {
-//   const [open, setOpen] = React.useState(false);
-//   const [showDeleteBtn, setDeleteBtn] = React.useState(false);
+const MusicAsset = ({ audioUrl }: any) => {
+  // const [open, setOpen] = React.useState(false);
+  // const [showDeleteBtn, setDeleteBtn] = React.useState(false);
 
-//   const showDeleteButton = () => {
-//     setDeleteBtn(true);
-//   };
-//   const hideDeleteButton = () => {
-//     setDeleteBtn(false);
-//   };
-//   const deleteAction = () => {
-//     deleteAsset(asset._id);
-//   };
-//   const openDeleteDialog = () => {
-//     setDeleteBtn(false);
-//     setOpen(true);
-//   };
-//   const closeDeleteDialog = () => {
-//     setOpen(false);
-//   };
-//   return (
-//     <div
-//       className="wrapperImgLib"
-//       onMouseEnter={showDeleteButton}
-//       onMouseLeave={hideDeleteButton}
-//     >
-//       <DeleteDialog
-//         open={open}
-//         closeDeleteDialog={closeDeleteDialog}
-//         actionDelete={deleteAction}
-//         isDeleting={isDeletingAsset}
-//         textContent="You will not be able to recover this asset once it is deleted"
-//       />
-//       {asset.type === "logo" ? (
-//         <img
-//           src={asset.url}
-//           crossOrigin="anonymous"
-//           className="imgAssetLib"
-//           alt="asset"
-//         />
-//       ) : (
-//         <img src={asset.url} className="imgAssetLib" alt="asset" />
-//       )}
-//       <button
-//         onClick={openDeleteDialog}
-//         className={showDeleteBtn ? "showDeleteBtn" : "hideDeleteBtn"}
-//       >
-//         <Tooltip placement="top" title="Delete" arrow>
-//           <span>
-//             <RiDeleteBack2Line size={"1.5em"} />
-//           </span>
-//         </Tooltip>
-//       </button>
-//     </div>
-//   );
-// };
+  // const showDeleteButton = () => {
+  //   setDeleteBtn(true);
+  // };
+  // const hideDeleteButton = () => {
+  //   setDeleteBtn(false);
+  // };
+  // const deleteAction = () => {
+  //   deleteAsset(asset._id);
+  // };
+  // const openDeleteDialog = () => {
+  //   setDeleteBtn(false);
+  //   setOpen(true);
+  // };
+  // const closeDeleteDialog = () => {
+  //   setOpen(false);
+  // };
+  return (
+    <div className="wrapperAudioLib">
+      <audio src={audioUrl} controls />
+    </div>
+  );
+};
