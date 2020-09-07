02f146c02bae28177a262fc787aa955f7302a19d
Asad Raza <basitsattar850@gmail.com>
2020-09-07 16:20:14 +0500

    display dummy templates for campaign

--- src/Screens/CampaignNew/SelectTemplate.tsx
+++ src/Screens/CampaignNew/SelectTemplate.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Button } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
+import CampaignTemplateCard from "../../components/CampaignTemplateCard";
 
 interface IProps {
     moveToNextStep: () => void;
@@ -7,8 +8,23 @@ interface IProps {
 class SelectTemplate extends React.Component<IProps> {
     render() {
         return <div className="wrapperSelectTemplate">
-            <h3>Select template to for your Campaign</h3>
-            <Button onClick={this.props.moveToNextStep}>Click to Proceed</Button>
+            <Typography variant="h4" >
+                Select template for your Campaign
+      </Typography>
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={12} md={3} lg={3} >
+                    <CampaignTemplateCard moveToNextStep={this.props.moveToNextStep} />
+                </Grid>
+                <Grid item xs={12} sm={12} md={3} lg={3} >
+                    <CampaignTemplateCard moveToNextStep={this.props.moveToNextStep} />
+                </Grid>
+                <Grid item xs={12} sm={12} md={3} lg={3} >
+                    <CampaignTemplateCard moveToNextStep={this.props.moveToNextStep} />
+                </Grid>
+                <Grid item xs={12} sm={12} md={3} lg={3} >
+                    <CampaignTemplateCard moveToNextStep={this.props.moveToNextStep} />
+                </Grid>
+            </Grid>
         </div>
     }
 }
--- src/components/CampaignTemplateCard/index.tsx
+++ src/components/CampaignTemplateCard/index.tsx
@@ -1,8 +1,35 @@
 import React from "react";
-import { Card, CardActionArea, CardActions, CardMedia, Typography } from "@material-ui/core";
+import { Card, CardActionArea, CardActions, CardContent, Button, CardMedia, Typography } from "@material-ui/core";
 
-const CampaignTemplateCard: React.FC = () => {
-    return <div></div>
+
+interface IProps {
+    moveToNextStep: () => void;
+}
+const CampaignTemplateCard: React.FC<IProps> = ({ moveToNextStep }) => {
+    return <Card >
+        <CardActionArea>
+            <CardMedia
+                component="img"
+                alt="Template Thumbnail"
+                height="140"
+                image="https://media.istockphoto.com/photos/law-should-know-concept-the-lawyer-explained-to-the-client-to-plan-picture-id944503634?k=6&m=944503634&s=612x612&w=0&h=sLdq_w0yE062amiwD04H-HkWYVptoMMhkqKvSEdm_nA="
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                    Lawyer
+        </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                    across all continents except Antarctica
+        </Typography>
+            </CardContent>
+        </CardActionArea>
+        <CardActions>
+            <Button size="small" color="primary" onClick={moveToNextStep}>
+                Proceed
+      </Button>
+        </CardActions>
+    </Card>
 }
 
 export default CampaignTemplateCard;
