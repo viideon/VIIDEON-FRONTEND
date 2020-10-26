840c223fc891fdb2b5a10be109867e80565a9953
M Hamza <hafiz.quraishi.official@gmail.com>
2020-10-26 21:39:04 +0500

    link

--- src/lib/Api.ts
+++ src/lib/Api.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 const baseURL = process.env.REACT_APP_APIURL;
 
 const API = axios.create({
-  // baseURL,
-  baseURL: "http://192.168.18.8:3008",
+  baseURL,
+  // baseURL: "http://192.168.18.8:3008",
   // baseURL: "http://192.168.10.5:3008",
   timeout: 40000
 });
